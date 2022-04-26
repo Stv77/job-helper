275aa945b0e8a5a9e3fc2bea63ff234e35aa98f6
Stevanus Lieberto <stevanuslieberto@gmail.com>
2022-04-26 15:30:15 +0700

    'updated'

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from errno import ENOENT
+from hashlib import new
 from msilib.schema import Error
+import re
+import time
 import nltk
-#nltk.download("stopwords") -> run once
 from nltk.corpus import stopwords
-import selenium
+from selenium import webdriver
 import requests
 import wget
 import zipfile
 import os
+import requests
+from requests_html import HTMLSession
+
 
 id_stopwords = stopwords.words('indonesian')
 en_stopwords = stopwords.words('english')
@@ -34,8 +39,68 @@ def extract_webdriver(extract_location):
 
 destination_extract = r'.\Lib\site-packages\selenium\webdriver\chrome'
 
-#checking the existence of webdriver
+#checking the existence of webdriver & download nltk stopwrods 1st time
 if(os.path.isfile('webdriverInstalled.txt')):
     print("Webdriver has been installed!")
 else:
     extract_webdriver(destination_extract)
+    nltk.download("stopwords")
+
+#driver
+options = webdriver.ChromeOptions()
+options.add_argument(open("dumping_ground.txt", "r").read())
+driver = webdriver.Chrome(executable_path=destination_extract+ r"\chromedriver.exe", chrome_options=options)
+
+def link(url):
+    session = HTMLSession()
+    response = session.get(url)
+
+    links_html = response.html.absolute_links
+    storing = """"""
+    for x in links_html:
+        storing+=x+"\n"
+    new_links = re.compile("https://www.jobstreet.co.id/id/job/\w.+")
+    return new_links.findall(storing)
+
+def get_links(driver):
+    #Open the page to get the biggest page number
+    main_link = 'https://www.jobstreet.co.id/id/job-search/staff-notaris-jobs/'
+    driver.maximize_window()
+    driver.get(main_link)
+
+    container_num = []
+
+    #Get the biggest page number
+    num_pag = driver.find_elements_by_css_selector('select.sx2jih0.sx2jih1 > option')
+    for num_pags in num_pag:
+        container_num.append(num_pags.text)
+
+    biggest_num = container_num[-1]
+
+    final_lists = []
+    #Pagination
+    for x in range(int(biggest_num)):
+        #First page: get the link
+        if x == 0:
+            copy_list = link(url=main_link).copy()
+            for x in copy_list:
+                final_lists.append(x)
+        #The rest: reload and get the link
+        elif x >= 1:
+            new_link = main_link +str(x+1)+'/'
+            driver.get(new_link)
+            time.sleep(3)
+            copy_list = link(url=new_link).copy()
+            for x in copy_list:
+                final_lists.append(x)
+
+    # Store the links in txt file
+    if len(final_lists) == 0:
+        pass
+    elif len(final_lists) > 0:
+        with open('list_of_links.txt','w') as l:
+            for items in final_lists:
+                l.write('%s\n' % items)
+    driver.quit()
+
+get_links(driver=driver)
